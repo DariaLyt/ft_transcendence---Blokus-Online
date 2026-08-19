4a737441760d2df9ed85033ce2279f45e105e665
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-08-19 15:47:22 +0300

    feat: added lobby page with only placeholder info

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GamePage from './pages/GamePage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
+import LobbyPage from './pages/LobbyPage';
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
               {/* 3. The actual rules: */}
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/game" element={<GamePage />} />
+                <Route path="/lobby" element={<LobbyPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
             </Routes>
         </BrowserRouter>
