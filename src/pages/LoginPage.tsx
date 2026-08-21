9c4d30cf000ef6e7b68fdad7f05bc0ef807905a8
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-08-21 14:57:21 +0300

    feat: added React hook to keep track of what user types

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react"; // React hook that lets a component store and update state, we use it to keep track of what the user types
+
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-slate-100">
             <div className="bg-white p-8 rounded-xl shadow-md">
@@ -6,14 +11,20 @@ export default function LoginPage() {
                 <p className="text-slate-500 mb-6">Enter your details to continue</p>
                 <div className="mb-4">
                     <label className="block text-sm font-medium text-slate-700 mb-1">Email</label>
-                    <input type="email" className="w-full border border-slate-300 rounded-lg px-3 py-2"/>
+                    <input type="email"
+                        value={email} //connects input to React state
+                        onChange={(e) => setEmail(e.target.value)} // event handler that runs when input changes
+                        className="w-full border border-slate-300 rounded-lg px-3 py-2"/>
                 </div>
 
                 <div className="mb-4">
                     <label className="block text-sm font-medium text-slate-700 mb-1">Password</label>
-                    <input type="password" className="w-full border border-slate-300 rounded-lg px-3 py-2"/>
+                    <input type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full border border-slate-300 rounded-lg px-3 py-2"/>
                 </div>
-                
+
                 <button type="button" className="w-full bg-blue-700 text-white rounded-lg px-3 py-2 font-medium">Log in</button>
                 <p className="text-sm text-slate-500 text-center mt-4">Don't have an account? Register here</p>
             </div>
