3bf1f4ddbbc4358c43714cdd68eda6ab3478dbb9
Nuria Mascaro Triay <nmascaro@student.hive.fi>
2026-08-27 12:00:02 +0300

    fix: removed lobby page route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GamePage from './pages/GamePage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
-import LobbyPage from './pages/LobbyPage';
 import RegisterPage from './pages/RegisterPage';
 import Menu from './pages/Menu';
 import Matchmaking from './pages/Matchmaking';
@@ -17,7 +16,6 @@ export default function App() {
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/menu" element={<Menu />} />
                 <Route path="/matchmaking" element={<Matchmaking />} />
-                <Route path="/lobby" element={<LobbyPage />} />
                 <Route path="/game" element={<GamePage />} />
                 <Route path="/profile" element={<ProfilePage />} />
             </Routes>
