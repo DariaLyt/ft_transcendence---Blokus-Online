9c47f7ceb0dc8171b979f0415d8b0057a0a08e34
Cheow Yuen Wong <cwong@c3r1p7.hive.fi>
2026-08-31 14:15:55 +0300

    integrate with frontend

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -11,7 +11,7 @@ export default function RegisterPage() {
 
     const handleRegister = async () => {
         const response = await fetch (
-            "http://localhost:3000/api/auth/register",
+            "https://localhost:3000/api/auth/register",
             {
                 method: "POST",
                 headers: {
